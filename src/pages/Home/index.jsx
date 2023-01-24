ec274ddf3db774764c50656d50978899e4708de3
rommelsanthiago <rommel.santhiago@gmail.com>
2023-01-24 13:07:50 -0300

    Vercel config fix

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useMemo } from 'react'
+import React, { useContext, useState, useMemo } from 'react'
 import { useNavigate } from "react-router-dom"
 import { ShoppingBag } from 'react-feather';
 
